fca7f40679283e9e9cdb5af9b38a69aeb679c83c
Makis Grammenos <31040056+makisgrammenos@users.noreply.github.com>
2021-07-18 15:56:29 +0300

    final changes

--- modules/PostQR.js
+++ modules/PostQR.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import 'axios'
 import axios from 'axios';
-function PostQR(data){
+import QRCode from 'react-native-qrcode-svg';
+ async function PostQR(data){
     var broken_link = data.split('/')
-
+    var showLink = `https://dilosi.services.gov.gr/show/${broken_link[broken_link.length-1]}/`
     var requestURL = `https://dilosi.services.gov.gr/api/declarations/${broken_link[broken_link.length-1]}/`
-    console.log(requestURL)
-    
-    axios.get(requestURL).then(response =>{
-        console.log(response)
-    }).catch(err=>{
-        console.log(err)
+   
+    var info;
+   await  axios.get(requestURL).then(function(response){
+        const {first_name_el,last_name_el,first_name_en,last_name_en} = response.data.extracted_step_info.fields;
+        return {'first_name_el':first_name_el.value,'last_name_el':last_name_el.value,'first_name_en':first_name_en.value,'last_name_en':last_name_en.value}
+        
+    }).then(data=> info = data).catch(err=>{
+        alert(err)
     })
+    
+    return {'data':info,'qr':showLink}
 }
 
 export default PostQR;
