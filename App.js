c9b7658d542fd76e2c8a8f4175953c924a03d266
Makis Grammenos <31040056+makisgrammenos@users.noreply.github.com>
2021-07-24 21:13:20 +0300

    update

--- App.js
+++ App.js
@@ -1,36 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import ScanScreen  from './screens/Scan';
 import VerifyScan from './screens/VerifyScan';
+import Certificate from './screens/Certificate';
 import { createStackNavigator } from '@react-navigation/stack';
-export default function App() {
+import * as SecureStore from 'expo-secure-store';
+export default function App({navigation}) {
   const Stack  = createStackNavigator();
-  return (
-   
+  const [check,setCheck] = useState(false);
+  const [certExists,setCertExists] = useState(false);
+  var cert = null;
+  useEffect(()=>{
+    try{
+     SecureStore.getItemAsync('certificate').then(value=>{
+       if (value!=null){
+         cert = JSON.parse(value);
+        
+       }
+     });
       
-     <>
-     
-      <NavigationContainer>
-       
-        <Stack.Navigator initialRouteName="QRSCAN">
-          <Stack.Screen name="QRSCAN" component={ScanScreen} options={{headerTitle:"Εισαγωγη Πιστοποιτηκού",headerStyle:{
-            backgroundColor:"#003476"    },
-            headerTintColor: 'white',
-            headerTitleStyle:'bold',
-            headerTitleAlign:'center'
-          }}/>
-          <Stack.Screen name ="VerifyScan" component={VerifyScan} options={{headerTitle:"Επιβεβαίωση στοιχείων",headerStyle:{
-            backgroundColor:"#003476"    },
-            headerTintColor: 'white',
-            headerTitleStyle:'bold',
-            headerTitleAlign:'center'}}/>
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
-   
-  );
+    }
+    catch(e){
+      alert(e)
+    }
+    if(cert!=null){
+      setCertExists(true);
+    }
+    setCheck(true);
+    console.log(check)
+    console.log(certExists)
+    console.log(cert)
+  },[check])
+  if (!check){
+    return(
+      <View>
+        <Text>Loading</Text>
+      </View>
+    )
+  }
+  else{
+    return (
+    
+        
+      <>
+      
+        <NavigationContainer>
+        
+          <Stack.Navigator initialRouteName={certExists?"Certificate":'QRSCAN'}>
+            <Stack.Screen name="QRSCAN" component={ScanScreen} options={{headerTitle:"Εισαγωγη Πιστοποιτηκού",headerStyle:{
+              backgroundColor:"#003476"    },
+              headerTintColor: 'white',
+              headerTitleStyle:'bold',
+              headerTitleAlign:'center'
+            }}/>
+            <Stack.Screen name ="VerifyScan" component={VerifyScan} options={{headerTitle:"Επιβεβαίωση στοιχείων",headerStyle:{
+              backgroundColor:"#003476"    },
+              headerTintColor: 'white',
+              headerTitleStyle:'bold',
+              headerTitleAlign:'center'}}/>
+            <Stack.Screen name="Certificate" options={{headerTitle:"Πιστοποιτηκό εμβολιασμού COVID-19",headerStyle:{
+              backgroundColor:"#003476"    },
+              headerTintColor: 'white',
+              headerTitleStyle:'bold',
+              headerTitleAlign:'center'}}>
+                {props => <Certificate {...props} cert={cert}/>}
+              </Stack.Screen>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </>
+    
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- screens/VerifyScan.js
+++ screens/VerifyScan.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import {View, Text, TouchableOpacity, StyleSheet,Button,Dimensions} from 'react-native';
+import React,{useState,useEffect} from 'react'
+import {View, Text, TouchableOpacity, StyleSheet,Button,Dimensions, DevSettings} from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 import {NativeBaseProvider ,Center,VStack,Heading} from  'native-base';
+import SaveCertificate from '../modules/SaveCertificate';
+
 function VerifyScan({route,navigation}) {
     const {data} = route.params;
-    
+    const [certSaved,setCertSaved] = useState(false);
+    useEffect(()=>{
+        if(certSaved){
+            DevSettings.reload()
+        }
+    },[certSaved])
+    const handleSubmition = (data) => {
+       if(SaveCertificate(data)){
+           setCertSaved(true);
+       }
+       else {
+           setCertSaved(false);
+
+        }
+}
     return(
         <NativeBaseProvider>
         {/* <Heading size={'md'} style={{alignItems:'center'}}>Βεβαίωση Εμβολιασμού COVID-19</Heading> */}
@@ -29,7 +45,7 @@ function VerifyScan({route,navigation}) {
             title="Αποθηκευση Πιστοποιητικου"
             color="#003476"
            
-            onPress={()=>{}}
+            onPress={()=>{handleSubmition(data)}}
             />
         </View>
        
